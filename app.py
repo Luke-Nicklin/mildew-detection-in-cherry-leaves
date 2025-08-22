b741b7d6e7be244881369f43284f24aa70950074
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-08-22 08:54:32 +0100

    Update streamlit pages header

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from app_pages.leaves_visualiser import leaves_visualiser_body
 from app_pages.powdery_mildew_detector import powdery_mildew_detector_body
 from app_pages.ml_performance import ml_performance_body
 
-app = MultiPage(app_name="Cherry Leaf Mildew Detector")
+app = MultiPage(app_name="Cherry Leaf Powdery Mildew Detector")
 
 # Add all pages here
 app.add_page("Summary", summary_body)
