427404f857473b8f4c5578ae3f6c1c53e27ad7f1
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-08-22 15:12:27 +0100

    Add content and images to hypotheses page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from app_pages.multipage import MultiPage  # Ensure MultiPage is defined in app_
 
 # Load all pages here
 from app_pages.summary import summary_body
-from app_pages.project_hypothesis import project_hypothesis_body
+from app_pages.project_hypotheses import project_hypotheses_body
 from app_pages.leaves_visualiser import leaves_visualiser_body
 from app_pages.powdery_mildew_detector import powdery_mildew_detector_body
 from app_pages.ml_performance import ml_performance_body
@@ -12,7 +12,7 @@ app = MultiPage(app_name="Cherry Leaf Powdery Mildew Detector")
 
 # Add all pages here
 app.add_page("Summary", summary_body)
-app.add_page("Project Hypothesis", project_hypothesis_body)
+app.add_page("Project Hypotheses", project_hypotheses_body)
 app.add_page("Leaves Visualiser", leaves_visualiser_body)
 app.add_page("Powdery Mildew Detector", powdery_mildew_detector_body)
 app.add_page("ML Performance", ml_performance_body)
---
+++ app_pages/project_hypotheses.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import image as imread
+
+def project_hypotheses_body():
+    """Display the project hypothesis page."""
+    st.title("Project Hypotheses")
+    st.write("This page outlines the hypotheses of the project related to " \
+    "cherry leaf powdery mildew detection.")
+
+    st.write("""
+        **Hypothesis 1:**
+             There is a noticeable visual difference between healthy 
+             cherry leaves and those infected with powdery mildew. This 
+             difference can be observed in features like texture, colour, 
+             and the presence of white, powdery spots.
+             This hypothesis links to business requirement 1.
+             """)
+    
+    powdery_mildew_image = plt.imread(
+        "streamlit_images/powdery-mildew.png"
+    )
+
+    st.image(
+        powdery_mildew_image, 
+        caption="Cherry Leaf with Powdery Mildew", 
+        use_container_width=True
+    )
+
+    healthy_leaf_image = plt.imread(
+        "streamlit_images/healthy-leaf.png"
+    )
+
+    st.image(
+        healthy_leaf_image, 
+        caption="Healthy Cherry Leaf", 
+        use_container_width=True
+    )
+    
+    st.success("""
+        Infected leaves have distinct marks compared to healthy leaves.
+        """)
+    
+    st.write("""
+        **Hypothesis 2:**
+             A machine learning model can be trained to accurately classify 
+             cherry leaves as either healthy or infected, achieving an 
+             accuracy of 97% or higher.
+             This hypothesis links to business requirement 2.
+            """)
+    
+    st.success("""
+        The model can classify cherry leaves with an accuracy higher than 97%.
+        """)
+               
+    st.write("""    
+        **Objectives:**
+        - To collect a dataset of cherry leaves with and without powdery mildew.
+        - To train machine learning models to classify the presence of powdery mildew.
+        - To evaluate the performance of these models using various metrics.
+        - To provide a user-friendly interface for detecting powdery mildew on cherry leaves.
+    """)
