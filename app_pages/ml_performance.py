2a8d0fcf7c35c531f2930c18de6ca786a54a0aca
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-08-26 11:50:28 +0100

    Add ml performance page. Add evaluation clf page. Fix incorrect code and python formatting errors

--- app_pages/ml_performance.py
+++ app_pages/ml_performance.py
@@ -1,18 +1,44 @@
 """Model performance page."""
 
 import streamlit as st
-import matplotlib.pyplot as plt
-from matplotlib import image as imread
-
+import os
+import pandas as pd
+from src.machine_learning.evaluate_clf import load_evaluation_metrics
 
 
 def ml_performance_body():
-    """Display the machine learning performance page."""
+    """ML performance metrics page."""
     st.title("Machine Learning Performance")
-    st.write("Below shows the performance metrics of the machine "
-             "learning models used in this project.")
-    
+
+    # Find the root directory
     st.write("### Train, Validation and Test Set: Labels Distribution")
-    st.image("streamlit_images/labels-distribution.png",
+    st.image(os.path.join("outputs", "v1", "label_distribution_bar.png"),
              caption="Labels Distribution in Train, Validation and Test Sets",
              use_container_width=True)
+
+    st.write("### Model history")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(os.path.join("outputs", "v1", "model_training_acc.png"),
+                 caption="Model training accuracy",
+                 use_container_width=True)
+    with col2:
+        st.image(os.path.join("outputs", "v1", "model_training_losses.png"),
+                 caption="Model training losses",
+                 use_container_width=True)
+
+    st.write("### Genralised performance on the test set")
+    version = "v1"
+    metrics_df = pd.DataFrame(
+        load_evaluation_metrics(version),
+        index=['Loss', 'Accuracy'],
+        columns=['Value']
+    )
+    st.dataframe(metrics_df)
+
+    st.write("---")
+
+    st.success("""
+        The model achieved an accuracy of 99.68% on the test set, indicating
+        strong generalisation capabilities.
+        """)
